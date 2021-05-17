91c660f9a29113c3bb1c552a4bb622f4fa8ebd96
Александр Перепадин <alyansalex@gmail.com>
2021-05-17 20:19:00 +0300

    First

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 19455;
+
+        int bonusMiles = ticketPrice / 20;
+
+        System.out.println("Сумма покупки:");
+        System.out.println(ticketPrice);
+
+        System.out.println("Всего начислено бонусных миль:");
+        System.out.println(bonusMiles);
+    }
+}
+
+
+
